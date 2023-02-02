679a8bd630d8a625d3d355ada8ac36ea8d434cc8
mixeus <mixeuskaczala@gmail.com>
2023-02-02 16:20:50 -0300

    stab-version

---
+++ slick.js
@@ -0,0 +1,12 @@
+$('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    speed: 600,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    button: false,
+  });
+  
